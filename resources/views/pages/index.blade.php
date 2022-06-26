b68b0fab7a4b580e6ae0dec24e93ed82f6222e49
Daba Clovis <dabaclovis@yahoo.com>
2022-06-26 07:41:49 -0400

    clovis: initial commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <img src="" alt="">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                        <h3>Welcome to my website</h3>
+                        <small><i class="fa fa-times" aria-hidden="true"></i></small>
+                    </div>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est temporibus saepe ex eius officiis iste sequi, error provident?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora saepe officia placeat.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <img src="" alt="">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                        <h3>Welcome to my website</h3>
+                        <small><i class="fa fa-times" aria-hidden="true"></i></small>
+                    </div>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est temporibus saepe ex eius officiis iste sequi, error provident?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora saepe officia placeat.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <img src="" alt="">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                        <h3>Welcome to my website</h3>
+                        <small><i class="fa fa-times" aria-hidden="true"></i></small>
+                    </div>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est temporibus saepe ex eius officiis iste sequi, error provident?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora saepe officia placeat.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
